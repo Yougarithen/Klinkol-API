469660cc2f714036ee3435df8d0ddec84b7336f1
Yougarithen <yougarithenissaadi@gmail.com>
2026-06-23 10:54:46 +0100

    maj des achats

--- src/controllers/ravitaillementController.js
+++ src/controllers/ravitaillementController.js
@@ -55,7 +55,7 @@ exports.getByPeriode = async (req, res) => {
 
 exports.create = async (req, res) => {
     try {
-        const { id_matiere, quantite, prix_achat, fournisseur, numero_bon_livraison, commentaire, responsable, date_ravitaillement } = req.body;
+        const { id_matiere, quantite, prix_achat, fournisseur, numero_bon_livraison, commentaire, responsable, date_ravitaillement, id_transporteur } = req.body;
 
         // Validation
         if (!id_matiere || !quantite || !responsable) {
@@ -80,7 +80,8 @@ exports.create = async (req, res) => {
             numero_bon_livraison,
             commentaire,
             responsable,
-            date_ravitaillement
+            date_ravitaillement,
+            id_transporteur
         });
 
         res.status(201).json({
